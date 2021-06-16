ce199d6e84066f740ed78792cb9b94df3c155276
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-16 22:08:24 +0900

    Chore: Practicing toggle event

--- click.js
+++ click.js
@@ -2,12 +2,7 @@ let h1 = document.querySelector("div.clicker:first-child h1")
 
 
 function handleH1Click(){
-  const clickedClass = "clicked"
-  if(h1.className === clickedClass){
-    h1.className = "";
-  } else {
-    h1.className = clickedClass
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleH1Click);
