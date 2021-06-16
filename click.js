c400ef5aa71ab1e06b13e46508c399398cc93764
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-16 21:35:54 +0900

    Chore: Adding style for event practice

--- click.js
+++ click.js
@@ -2,14 +2,12 @@ let h1 = document.querySelector("div.clicker:first-child h1")
 
 
 function handleH1Click(){
-  const currentColor = h1.style.color;
-  let newColor;
-  if(currentColor === 'red'){
-    newColor = 'blue'
-  }else {
-    newColor = "red";
+  const clickedClass = "clicked"
+  if(h1.className === clickedClass){
+    h1.className = "";
+  } else {
+    h1.className = clickedClass
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleH1Click);
