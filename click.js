79331d78841c5ca60b0f2ea490aab3e892265545
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-16 21:21:03 +0900

    Chore: Editing code for onclick event

--- click.js
+++ click.js
@@ -1,38 +1,15 @@
 let h1 = document.querySelector("div.clicker:first-child h1")
 
+
 function handleH1Click(){
-  h1.style.color = "red";
-}
-
-function handleMouseEnter(){
- h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
- h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-  alert("copier!")
-}
-
-function handleWindowOffline(){
-  alert("SOS no wifi!")
-}
-
-function handleWindowOnline(){
-  alert("ALL GOOOD")
+  const currentColor = h1.style.color;
+  let newColor;
+  if(currentColor === 'red'){
+    newColor = 'blue'
+  }else {
+    newColor = "red";
+  }
+  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleH1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
