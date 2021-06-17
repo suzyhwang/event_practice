3ebb8a9ee65dc0088bc9a2c01fa47866d1d8d752
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-18 00:28:18 +0900

    Chore: made savedusername function

--- click.js
+++ click.js
@@ -3,17 +3,26 @@ const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `Hello ${username}!`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-
-loginForm.addEventListener("submit", onLoginSubmit);
-
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+ paintGreetings(savedUsername);
+}
