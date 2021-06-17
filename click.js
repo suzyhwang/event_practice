9ecd60fd95f01b72e143ab124b77cd5de7e43f28
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-17 23:58:50 +0900

    Chore: greeting user practice

--- click.js
+++ click.js
@@ -1,12 +1,15 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  console.log(username);
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
