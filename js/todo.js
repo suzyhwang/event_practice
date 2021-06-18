797a8290658d2bf28862d08deb5ea859aba02b6c
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-18 23:57:47 +0900

    Chore: Started making to-do list

---
+++ js/todo.js
@@ -0,0 +1,13 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+
+function handleToDoSubmit(event){
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value="";
+}
+
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
